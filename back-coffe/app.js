437bfb519c1853031e6906d129e45f349ef9d9aa
ghamz-id <hcoustic@gmail.com>
2024-03-20 15:15:13 +0700

    setup

--- back-coffe/app.js
+++ back-coffe/app.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
-  //pakai ini hanya utk local
-  require("dotenv").config(); //Install environment env
+	//pakai ini hanya utk local
+	require("dotenv").config(); //Install environment env
 }
 
 const express = require("express");
@@ -18,45 +18,40 @@ app.use("/", router);
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
+	cors: {
+		origin: "*",
+	},
 });
 
 let users = [];
 let messages = [];
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-  
-  // handshake
-  console.log(socket.handshake.auth);
-  if(socket.handshake.auth.username) {
-    users.push(socket.handshake.auth.username)
-  }
+	console.log("A user connected", socket.id);
 
-  // emit server to all client
-  socket.emit("message:info", messages)
+	// handshake
+	console.log(socket.handshake.auth);
+	if (socket.handshake.auth.username) {
+		users.push(socket.handshake.auth.username);
+	}
 
-  // send users event to all client
-  io.emit("users", users)
+	// emit server to all client
+	socket.emit("message:info", messages);
 
-  socket.on("message:new", (param) => {
-    messages.push(param);
-    io.emit("message:info", param)
-  })
+	// send users event to all client
+	io.emit("users", users);
 
-  socket.on("disconnect", () => {
-    users = users.filter(user => {
-      return user !== socket.handshake.auth.username
-    })
-    io.emit('users', users)
-  })
+	socket.on("message:new", (param) => {
+		messages.push(param);
+		io.emit("message:info", param);
+	});
+
+	socket.on("disconnect", () => {
+		users = users.filter((user) => {
+			return user !== socket.handshake.auth.username;
+		});
+		io.emit("users", users);
+	});
 });
 
-
-httpServer.listen(port, () => {
-  console.log("Server is running on port", port)
-})
-
-module.exports = app;
+module.exports = httpServer;
