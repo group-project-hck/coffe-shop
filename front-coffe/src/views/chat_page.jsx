92ba54f3e5b4ba7c28b41ef804f1e71958f27b8d
naufalsoerya <naufalsurya.official@gmail.com>
2024-03-20 10:43:24 +0700

    update: socket.io (global version fix)

--- front-coffe/src/views/chat_page.jsx
+++ front-coffe/src/views/chat_page.jsx
@@ -126,7 +126,7 @@ export default function Chat_Page() {
                       ) : (
                         <div className="col-start-6 col-end-13 p-3 rounded-lg">
                           <div className="flex items-center justify-start flex-row-reverse">
-                            <div className="flex items-center justify-center h-4 w-4 bg-yellow-600 rounded-full">{el.from}</div>
+                            <div className="flex items-center justify-center h-4 w-4 bg-yellow-600 rounded-full"></div>
                             <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
                               <div>{el.message}</div>
                             </div>
