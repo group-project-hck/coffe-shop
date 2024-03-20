585b8bc5c3bbaada086e24422be5a8ded001c19c
Hafidh Ageng Anindito <hafidhdito007@gmail.com>
2024-03-20 14:35:41 +0700

    group-project

--- front-coffe/src/views/chat_page.jsx
+++ front-coffe/src/views/chat_page.jsx
@@ -1,4 +1,71 @@
+import { io } from "socket.io-client";
+import { BASE_URL } from "../../constants";
+import { useEffect, useState } from "react";
+
+const socket = io(`${BASE_URL}`);
+
 export default function Chat_Page() {
+  const [emotes, setEmotes] = useState([
+    "😊",
+    "😂",
+    "😍",
+    "👍",
+    "😁",
+    "😅",
+    "😅",
+    "😅",
+  ]);
+  const [showEmoji, setShowEmoji] = useState(false);
+
+  const addEmoteToMessage = (emote) => {
+    setNewMessage((prevInput) => prevInput + emote);
+    setShowEmoji(false);
+  };
+  const [users, setUsers] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.disconnect();
+    socket.auth = {
+      username: localStorage.username,
+    };
+
+    socket.connect();
+  }, []);
+
+  useEffect(() => {
+    socket.on("users", (param) => {
+      setUsers(param);
+    });
+
+    socket.on("message:info", (param) => {
+      setMessages(messages.concat(param));
+    });
+
+    return () => {
+      socket.off("users");
+      socket.off("message:info");
+    };
+  }, [messages]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    try {
+      socket.emit("message:new", {
+        from: localStorage.username,
+        message: newMessage,
+      });
+
+      setNewMessage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(messages);
+  console.log(users, "<<<<<<<<<<<<<<<<<<<<<<<");
   return (
     <>
       {/* component */}
@@ -68,42 +135,26 @@ export default function Chat_Page() {
                             </div>
                           </div>
                         </div>
-                        <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                          <div>I'm ok what about you?</div>
+                      ) : (
+                        <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                          <div className="flex flex-row items-center">
+                            <div className="flex items-center justify-center h-4 w-4 bg-blue-600 rounded-full"></div>
+                            <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                              <div>{el.message}</div>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div> 
+                      );
+                    })}
                   </div>
                 </div>
               </div>
-              <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
-                <div>
-                  <button className="flex items-center justify-center text-gray-400 hover:text-gray-600">
-                    <svg
-                      className="w-5 h-5"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                      />
-                    </svg>
-                  </button>
-                </div>
-                <div className="flex-grow ml-4">
-                  <div className="relative w-full">
-                    <input
-                      type="text"
-                      className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
-                    />
-                    <button className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+                  <div>
+                    <button className="flex items-center justify-center text-gray-400 hover:text-gray-600">
                       <svg
-                        className="w-6 h-6"
+                        className="w-5 h-5"
                         fill="none"
                         stroke="currentColor"
                         viewBox="0 0 24 24"
@@ -113,34 +164,79 @@ export default function Chat_Page() {
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth={2}
-                          d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
                         />
                       </svg>
                     </button>
                   </div>
-                </div>
-                <div className="ml-4">
-                  <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
-                    <span>Send</span>
-                    <span className="ml-2">
-                      <svg
-                        className="w-4 h-4 transform rotate-45 -mt-px"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
+                  <div className="flex-grow ml-4">
+                    <div className="relative w-full">
+                      <input
+                        type="text"
+                        className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                        onChange={(e) => setNewMessage(e.target.value)}
+                        value={newMessage}
+                      />
+                      <button
+                        type="button"
+                        onClick={() => setShowEmoji((v) => !v)}
+                        className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                        />
-                      </svg>
-                    </span>
-                  </button>
+                        <svg
+                          className="w-6 h-6"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  {showEmoji ? (
+                    <div className="emotes-container">
+                      {emotes.map((emote, index) => (
+                        <button
+                          key={index}
+                          onClick={() => addEmoteToMessage(emote)}
+                        >
+                          {emote}
+                        </button>
+                      ))}
+                    </div>
+                  ) : null}
+                  <div className="ml-4">
+                    <button
+                      className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                      type="submit"
+                    >
+                      <span>Send</span>
+                      <span className="ml-2">
+                        <svg
+                          className="w-4 h-4 transform rotate-45 -mt-px"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                          />
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
