17dc59e35c7589d2470af36ec933f7bc0ff4bd57
ghamz-id <hcoustic@gmail.com>
2024-03-21 12:08:56 +0700

    merge

--- front-coffe/src/components/avatar.jsx
+++ front-coffe/src/components/avatar.jsx
@@ -1,56 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Avatar() {
-  const navigate = useNavigate();
-  const Logout = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("username");
-    navigate("/");
-  };
+	const navigate = useNavigate();
+	const Logout = (e) => {
+		e.preventDefault();
+		localStorage.removeItem("access_token");
+		localStorage.removeItem("username");
+		navigate("/");
+	};
 
-<<<<<<< HEAD
-  return (
-    <>
-      <div className="dropdown dropdown-end text-black">
-        <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-ghost btn-circle avatar"
-        >
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
-          </div>
-        </div>
-        <ul
-          tabIndex={0}
-          className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 gap-2"
-        >
-          <li>
-            <Link to={"/home"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"/chat"}>Chat</Link>
-          </li>
-          {localStorage.username === "admin" && (
-            <li>
-              <Link to={"/products"}>Products</Link>
-            </li>
-          )}
-          <li>
-            <button onClick={Logout}>Logout</button>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
-=======
 	return (
 		<>
-			<div className="dropdown dropdown-end">
+			<div className="dropdown dropdown-end text-black">
 				<div
 					tabIndex={0}
 					role="button"
@@ -89,5 +50,4 @@ export default function Avatar() {
 			</div>
 		</>
 	);
->>>>>>> 6f9e3979570299d791690b74d89695ab563c70a2
 }
